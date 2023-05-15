81f06f3bc3d588427d6eb954284fa9afdf5f0d3a
Fedi Ghourabi <fedisaid.ghourabi@esprit.tn>
2023-05-15 20:13:40 +0100

    Added a LocalDateConverter from String to Date format to use it in Creating/Updating menu endpoint

--- src/main/java/com/wevioo/cantine/config/LocalDateConverter.java
+++ src/main/java/com/wevioo/cantine/config/LocalDateConverter.java
@@ -1,2 +1,18 @@
-package com.wevioo.cantine.config;public class LocalDateConverter {
+package com.wevioo.cantine.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class LocalDateConverter implements Converter<String, LocalDate> {
+
+    private static final String DATE_PATTERN = "yyyy-MM-dd";
+
+    @Override
+    public LocalDate convert(String source) {
+        return LocalDate.parse(source, DateTimeFormatter.ofPattern(DATE_PATTERN));
+    }
 }
